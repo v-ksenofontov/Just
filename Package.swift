6f990bd1afb7cf2cce3d82fc103f9c2506452a76
v-ksenofontov <sl76k@mail.ru>
2024-06-19 00:17:54 +0300

    Update Package.swift
    Package : add static library

--- Package.swift
+++ Package.swift
@@ -5,7 +5,9 @@ import PackageDescription
 let package = Package(
     name: "Just",
     products: [
-        .library(name: "Just", targets: ["Just"])
+        .library(name: "Just", targets: ["Just"]),
+        .library(name: "Just-Static", type: .static, targets: ["Just"]),
+        .library(name: "Just-Dynamic", type: .dynamic, targets: ["Just"])
     ],
     targets: [
         .target(name: "Just"),
